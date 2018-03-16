dfc8e2926f411fd5287add3cd0ab04d064386d5b
mattb20 <34608245+mattb20@users.noreply.github.com>
2018-03-16 20:14:52 +0000

    second commit

---
+++ wcteamselector.rb
@@ -0,0 +1,15 @@
+@teams = ["Russia", "Brazil", "Iran", "Japan", "Mexico", "Belgium", "South Korea", "Saudia Arabia",
+  "Germany", "England", "Spain", "Nigeria", "Costa Rica",
+"Poland", "Egypt", "Iceland", "Serbia", "Portugal", "France", "Uruguay", "Argentina",
+"Colombia"   ]
+@
+
+def greet_manager(manager)
+  puts "Hello, #{manager}"
+end
+
+def print_players
+end
+
+def add_player_to_squad
+end
