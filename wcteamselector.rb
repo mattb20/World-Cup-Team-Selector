be4634038a12bcfcafd5fb80c720b4d8afd52b49
mattb20 <34608245+mattb20@users.noreply.github.com>
2018-03-16 20:20:13 +0000

    beginning to build in information on the teams that can be used in the program

--- wcteamselector.rb
+++ wcteamselector.rb
@@ -1,8 +1,15 @@
 @teams = ["Russia", "Brazil", "Iran", "Japan", "Mexico", "Belgium", "South Korea", "Saudia Arabia",
   "Germany", "England", "Spain", "Nigeria", "Costa Rica",
 "Poland", "Egypt", "Iceland", "Serbia", "Portugal", "France", "Uruguay", "Argentina",
-"Colombia"   ]
-@
+"Colombia", "Panama", "Senegal", "Morocco", "Tunisia", "Switzerland"
+"Croatia", "Sweden", "Denmark", "Australia", "Peru"   ]
+@team_managers = [{country: "Russia", manager:},
+{country:, manager:},{country:, manager:},{country:, manager:},
+{country:, manager:},{country:, manager:},{country:, manager:},
+{country:, manager:},{country:, manager:},{country:, manager:},{country:, manager:},
+{country:, manager:},{country:, manager:},{country:, manager:},{country:, manager:},
+{country:, manager:},{country:, manager:},
+{country:, manager:},{country:, manager:},{country:, manager:},]
 
 def greet_manager(manager)
   puts "Hello, #{manager}"
