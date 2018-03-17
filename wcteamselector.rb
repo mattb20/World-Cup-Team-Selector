4327bdc08470c65c9a8497527ab1aebb61e5d4c0
mattb20 <34608245+mattb20@users.noreply.github.com>
2018-03-17 19:56:31 +0000

    added method for manager to choose formation or to enter custom one

--- wcteamselector.rb
+++ wcteamselector.rb
@@ -7,14 +7,32 @@
   "Juan Carlos Osorio", "Roberto Martinez", "Shin Tae-yong", "Juan Antonio Pizzi", "Joachim Lowe",
 "Gareth Southgate", "Julen Lopetegui", "Gernot Rohr", "Oscar Ramirez", "Adam Nawalka", "Hector Cuper",
  "Heimir Hallgrimsson", "Mladen Krstajic", "Fernando Santos", "Didier Deschamps", "Oscar Tabarez", "Jorge Sampaoli",]
-
+@formations = ["4 - 4 - 2", ]
 
 def interactive_menu
   puts "Hello, which team do you manage?"
   nation = gets.chomp
   greet_manager(nation)
+  choose_formation
 end
+def input_formation
+  puts "Please enter the formation you would like to add:"
+  @formations
 
+end
+def choose_formation
+  puts "Please choose your formation or enter custom to input a custom one:"
+    @formations.each.with_index do |formation_choice, index|
+      puts "#{index + 1}: #{formation_choice}"
+    end
+    manager_choice = gets.chomp
+      if manager_choice.downcase == "custom"
+        input_formation
+      else
+        @formations.at(manager choice.to_i - 1)
+      end
+
+end
 def greet_manager(nation)
   puts "Hello, #{@team_managers[@teams.find_index(nation)]}"
 end
