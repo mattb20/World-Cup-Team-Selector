727f2a273e70b41000f12d928da0b7030181aa8c
mattb20 <34608245+mattb20@users.noreply.github.com>
2018-03-17 19:34:27 +0000

    added array of teams, managers and method that greets manager

--- wcteamselector.rb
+++ wcteamselector.rb
@@ -1,18 +1,22 @@
 @teams = ["Russia", "Brazil", "Iran", "Japan", "Mexico", "Belgium", "South Korea", "Saudia Arabia",
   "Germany", "England", "Spain", "Nigeria", "Costa Rica",
 "Poland", "Egypt", "Iceland", "Serbia", "Portugal", "France", "Uruguay", "Argentina",
-"Colombia", "Panama", "Senegal", "Morocco", "Tunisia", "Switzerland"
+"Colombia", "Panama", "Senegal", "Morocco", "Tunisia", "Switzerland",
 "Croatia", "Sweden", "Denmark", "Australia", "Peru"   ]
-@team_managers = [{country: "Russia", manager:},
-{country:, manager:},{country:, manager:},{country:, manager:},
-{country:, manager:},{country:, manager:},{country:, manager:},
-{country:, manager:},{country:, manager:},{country:, manager:},{country:, manager:},
-{country:, manager:},{country:, manager:},{country:, manager:},{country:, manager:},
-{country:, manager:},{country:, manager:},
-{country:, manager:},{country:, manager:},{country:, manager:},]
+@team_managers =["Stanislav Cherchesov", "Tite", "Carloz Queiroz", "Vahid Halilhodzic",
+  "Juan Carlos Osorio", "Roberto Martinez", "Shin Tae-yong", "Juan Antonio Pizzi", "Joachim Lowe",
+"Gareth Southgate", "Julen Lopetegui", "Gernot Rohr", "Oscar Ramirez", "Adam Nawalka", "Hector Cuper",
+ "Heimir Hallgrimsson", "Mladen Krstajic", "Fernando Santos", "Didier Deschamps", "Oscar Tabarez", "Jorge Sampaoli",]
 
-def greet_manager(manager)
-  puts "Hello, #{manager}"
+
+def interactive_menu
+  puts "Hello, which team do you manage?"
+  nation = gets.chomp
+  greet_manager(nation)
+end
+
+def greet_manager(nation)
+  puts "Hello, #{@team_managers[@teams.find_index(nation)]}"
 end
 
 def print_players
@@ -20,3 +24,5 @@ end
 
 def add_player_to_squad
 end
+
+interactive_menu
